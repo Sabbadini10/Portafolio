c4a802c85528902c0e3e5be68645f31a85805951
Sabbadini10 <matiassabbadini24@gmail.com>
2022-12-28 17:18:58 -0300

    pagina skills terminada

--- src/components/skill/ComponentsSkill.jsx
+++ src/components/skill/ComponentsSkill.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import './Skill.css'
-import skill1 from '../../imagenes/skills/1_fASRfi5Gh3iNaJ5ejpxRmg.png'
-import skill2 from '../../imagenes/skills/450_1000-removebg-preview.png'
-import skill3 from '../../imagenes/skills/bootstrap-logo.png'
+import skill1 from '../../imagenes/skills/EXPRESSJS.png'
+import skill2 from '../../imagenes/skills/CSS3.png'
+import skill3 from '../../imagenes/skills/BOOTSTRAP.png'
 import skill4 from '../../imagenes/skills/html5.png'
 import skill5 from '../../imagenes/skills/javascript.png'
-import skill6 from '../../imagenes/skills/mysql-logo-1-820x450.png'
-import skill7 from '../../imagenes/skills/node-js-logo.png'
-import skill8 from '../../imagenes/skills/REACT-JS-KOCHI.png'
+import skill6 from '../../imagenes/skills/MYSQL.png'
+import skill7 from '../../imagenes/skills/NODEJS.png'
+import skill8 from '../../imagenes/skills/REACTJS.png'
+import skill9 from '../../imagenes/skills/SEQUELIZE.png'
+import skill10 from '../../imagenes/skills/GIT.png'
 import CardSkill from './CardSkills';
 
 
 function Skill() {
   return (
     <div className='Skill'>
-      <div className='contenedor-skill container-fluid d-flex flex-row justify-content-around align-items-center mt-3'>
+      <div className='contenedor-skill container-fluid d-flex flex-column justify-content-around align-items-center mt-3'>
       <div className='cardSkill-caja-1'>
     <CardSkill 
     src={skill1}
@@ -25,10 +27,8 @@ function Skill() {
     src={skill2}
     name="CSS3"
     link='' />
-    </div>
-   
-<div className='cardSkill-caja-1'>
-<CardSkill
+    
+    <CardSkill
 src={skill3}
 name="Boostrap"
 link='' />
@@ -46,10 +46,7 @@ link='' />
 src={skill6}
 name="MySQL"
     link='' />
-    
-</div>
-<div className='cardSkill-caja-1'>
-<CardSkill
+    <CardSkill
 src={skill7}
 name="Node.js"
 link='' />
@@ -57,7 +54,16 @@ link='' />
 src={skill8}
 name="React.js"
     link='' />
-    
+</div>
+<div className='cardSkill-caja-1'>
+<CardSkill
+src={skill9}
+name="Sequelize"
+link='' />
+<CardSkill 
+src={skill10}
+name="Git"
+    link='' />
 </div>
       </div>
     
