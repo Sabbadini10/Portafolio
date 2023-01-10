a4ca17567994b7b20ded977234c34ab827df26fc
Sabbadini10 <matiassabbadini24@gmail.com>
2023-01-10 17:07:01 -0300

    arregle error en formulario

--- src/components/formulario/ComponentsFormulario.jsx
+++ src/components/formulario/ComponentsFormulario.jsx
@@ -10,37 +10,37 @@ function ComponentsFormulario() {
   <div className="Contactos m-3 d-flex justify-content-center">
     <form class="contacto-formulario card m-3" method="POST" action="mailto:matiassabbadini24@gmail.com?subject=CONTACTO ENTREVISTA" enctype ="text/plain" >
       <div class="mb-3">
-        <label for="exampleFormControlInput2" class="form-label text-dark">
+        <label for="formularioName" class="form-label text-dark">
           Nombre
         </label>
         <input
-          type="email"
+          type="text"
           class="contacto-input form-control"
-          id="exampleFormControlInput"
+          id="formularioName"
           placeholder="Join Doin"
         />
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label text-dark">
+        <label for="formularioEmail" class="form-label text-dark">
           Email
         </label>
         <input
           type="email"
           class="contacto-input form-control"
-          id="exampleFormControlInput1"
+          id="formularioEmail"
           placeholder="JoinDoin@example.com"
         />
       </div>
       <div class="mb-3">
         <label
-          for="exampleFormControlTextarea1"
+          for="formularioTextarea"
           class="form-label text-dark"
         >
           Mensaje
         </label>
         <textarea
           class="contacto-textarea form-control"
-          id="exampleFormControlTextarea1"
+          id="formularioTextarea"
           rows="3"
         ></textarea>
       </div>
